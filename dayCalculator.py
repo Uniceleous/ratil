7eace6aa3bca0515d1ee462cb88aa98f43270693
Kunal <60289653+Uniceleous@users.noreply.github.com>
2023-02-26 17:57:07 +0530

    Add files via upload

--- dayCalculator.py
+++ dayCalculator.py
@@ -4,14 +4,13 @@ yyyy = 0
 f_date = None
 f_month = None
 f_year = None
-n_date = None
-n_month = None
-n_year1 = None
-n_year2 = None
 condition = None
+m_list1 = [1, 3, 5, 7, 8, 10, 12]
+m_list2 = [4, 6, 9, 11]
+m_list3 = 2
 
 def day():
-    global dd, mm, yyyy, f_date, f_month, f_year, n_date, n_month, n_year1, n_year2, condition
+    global dd, mm, yyyy, f_date, f_month, f_year, condition, m_list1, m_list2, m_list3
     inp = input("""
     Enter a date, and I will calculate the day for you!
     Example - 1/1/2023
@@ -22,19 +21,38 @@ def day():
     c_mm = mm.isnumeric()
     c_yyyy = yyyy.isnumeric()
 
-    if c_date: f_date = True
-    else: f_date = False
-    if c_mm: f_month = True
-    else: f_month = False
-    if c_yyyy: f_year = True
-    else: f_year = False
+    if c_date and c_mm and c_yyyy: 
+        if int(mm) <= 12 and int(mm) > 0:
+            if int(mm) in m_list1:
+                if int(dd) <= 31 and int(dd) > 0:
+                    f_date = True
+                else:
+                    f_date = False
+            elif int(mm) in m_list2:
+                if int(dd) <= 30 and int(dd) > 0:
+                    f_date = True
+                else:
+                    f_date = False
+            elif int(mm) == m_list3:
+                if int(dd) <= 28 and int(dd) > 0:
+                    f_date = True
+                else:
+                    f_date = False
+        else: 
+            f_month = False
+        if int(len(yyyy)) == 4 and int(yyyy) > 0:
+            f_year = True
+        else:
+            f_year = False
 
-    if f_date and f_month and f_year and len(dd) == 1 and len(mm) == 1 and len(yyyy) == 4: condition = True
-    else: print("False")
+        if f_date and f_month and f_year:
+            condition = True      
+        else:
+            condition = False
 
     if condition:
-        n_year1 = list(yyyy)
-        n_year1 = n_year1[2:0]
-        print(n_year1)
+        print(condition)
+    else:
+        print(False)
 
 day()
